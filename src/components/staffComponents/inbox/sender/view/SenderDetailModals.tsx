c55231f54df392ade3c2a79db24623e079a60c5f
Arpan sharma <sharmaarpan143@gmail.com>
2026-03-04 17:56:01 +0530

    working

--- src/components/staffComponents/inbox/sender/view/SenderDetailModals.tsx
+++ src/components/staffComponents/inbox/sender/view/SenderDetailModals.tsx
@@ -1,52 +1,29 @@
 "use client";
 
-import type { Company } from "@/app/staff-portal/inbox/types";
 import { ForwardModal } from "@/components/staffComponents/ForwardModal";
-
 
 interface SenderDetailModalsProps {
   forwardOpen: boolean;
   onCloseForward: () => void;
   forwardRefId: string | null;
-  servicesRequested: { name: string; id: string }[];
-  companyDirectory: Company[];
-  selectedCompany: Company | null;
-  onSelectCompany: (c: Company | null) => void;
-  onForward: (company: Company, customServices: string[] | null) => void;
-  onAddCompanyAndSelect: (name: string, email: string) => void;
-
-
-
+  onForward: (departmentIds: string[]) => void;
+  isForwardPending?: boolean;
 }
 
 export function SenderDetailModals({
   forwardOpen,
   onCloseForward,
   forwardRefId,
-  servicesRequested,
-  companyDirectory,
-  selectedCompany,
-  onSelectCompany,
   onForward,
-  onAddCompanyAndSelect,
-
-
+  isForwardPending = false,
 }: SenderDetailModalsProps) {
   return (
-    <>
-      <ForwardModal
-        isOpen={forwardOpen}
-        onClose={onCloseForward}
-        refId={forwardRefId}
-        servicesRequested={servicesRequested}
-        companyDirectory={companyDirectory}
-        selectedCompany={selectedCompany}
-        onSelectCompany={onSelectCompany}
-        onForward={onForward}
-        onAddCompanyAndSelect={onAddCompanyAndSelect}
-      />
-
-
-    </>
+    <ForwardModal
+      isOpen={forwardOpen}
+      onClose={onCloseForward}
+      refId={forwardRefId}
+      onForward={onForward}
+      isPending={isForwardPending}
+    />
   );
 }
